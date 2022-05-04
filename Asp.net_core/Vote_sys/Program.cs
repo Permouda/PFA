c3e17b6a5a48b848078b13f010c2c2a7ce5ab11e
Permouda <82866195+Permouda@users.noreply.github.com>
2022-05-04 17:07:05 +0000

    asp.net_core_packages

---
+++ Asp.net_core/Vote_sys/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Vote_sys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yesCounter = 5;
+            int noCounter = 3;
+            
+            // Les pourcentages
+            int total = yesCounter + noCounter;
+
+            var yesPercent = Math.Round(yesCounter * 100.0 / total) ;
+            var noPercent = noCounter * 100.0 / total;
+
+
+
+            Console.WriteLine($"Yes Counter: {yesCounter}, Percentage:{yesPercent}%");
+            Console.WriteLine($"No Counter: {noCounter}, Percentage:{noPercent}%");
+
+            if (yesCounter > noCounter)
+            {
+                Console.WriteLine($"Yes Won");
+            }else if (noCounter>yesCounter)
+            {
+                Console.WriteLine($"No Won");
+
+            }
+            else
+            {
+                Console.WriteLine($"DRAW");
+            }
+
+        }
+
+    }
+}
