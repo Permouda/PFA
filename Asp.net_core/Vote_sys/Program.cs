9bbbed96a50282414faecb582fd5a743e73d8608
Permouda <82866195+Permouda@users.noreply.github.com>
2022-05-06 14:47:35 +0000

    Winners Part_1_final

--- Asp.net_core/Vote_sys/Program.cs
+++ Asp.net_core/Vote_sys/Program.cs
@@ -7,31 +7,22 @@ namespace Vote_sys
 
     public class Counter
     {
-        // private string _name;
+        private double? _percentage;
         public Counter(string name, int count)
         {
             Name = name;
             Count = count;
         }
-        /*
-        public string GetName()
-        {
-            return _name;
-        }
-        public void SetName (string name)
-        {
-            _name = name;
-        }*/
-
-
         public string Name { get; }
         public int Count { get; private set; }
-
-        public double GetPercent(int total)
-        {
-            return Math.Round(Count * 100.0 / total);
-        }
-        public void Increment() => Count++;
+        public double GetPercent(int total) => _percentage ?? 
+            (_percentage = Math.Round(Count * 100.0 / total, 2)).Value;
+        
+        public void AddExcess(double excess) => _percentage += excess;
+        
+        
+        // had l'inc t7iyd apres
+        //public void Increment() => Count++;
     }
 
     public class CounterManager
@@ -43,17 +34,6 @@ namespace Vote_sys
         public List<Counter> Counters { get; set; }
 
         public int Total() => Counters.Sum(x => x.Count);
-        /*public int Total()
-        {
-           
-            
-            int total = 0;
-            foreach(var c in Counters)
-            {
-                total += c.Count;
-            }
-            return total;}
-            */
 
         public double TotalPercentage() => Counters.Sum(x => x.GetPercent(Total()));
 
@@ -61,35 +41,32 @@ namespace Vote_sys
             var excess = Math.Round(100 - TotalPercentage(), 2);
 
             Console.WriteLine($"Excess: {excess}");
-            /* var yesPercent = yes.GetPercent(manager.Total());
-             var noPercent = no.GetPercent(manager.Total());
-             var maybePercent = maybe.GetPercent(manager.Total());
-             */
 
             var biggestAmountOfVotes = Counters.Max(x => x.Count);
 
             var winners = Counters.Where(x => x.Count == biggestAmountOfVotes).ToList();
-
+            
             if(winners.Count == 1) {
                 var winner = winners.First();
+                winner.AddExcess(excess);
                 Console.WriteLine($"{winner.Name} Won");
-            }else if (winners.Count == Counters.Count)
+            }else 
             {
-                string.Join(" -DRAW- ", winners.Select(x => x.Name));
+                if (winners.Count != Counters.Count)
+                {
+                    var lowestAmountOfVotes = Counters.Min(x => x.Count);
+                    var loser = Counters.First(x => x.Count == biggestAmountOfVotes);
+                    loser.AddExcess(excess);
+                }
+                    Console.WriteLine(string.Join(" -DRAW- ", winners.Select(x => x.Name)));
+
             }
-            // Axcess
-
-
 
             foreach (var c in Counters)
             {
                 Console.WriteLine($"{c.Name} Counts: {c.Count}, Percentage: {c.GetPercent(Total())}%");
 
             }
-            /*Console.WriteLine($"Yes Counter: {yes.Count}, Percentage:{yesPercent}%");
-            Console.WriteLine($"No Counter: {no.Count}, Percentage:{noPercent}%");
-            Console.WriteLine($"Maybe Counter: {maybe.Count}, Percentage:{maybePercent}%");
-            */
             Console.WriteLine($"Total Percentage:{Math.Round(TotalPercentage(),2)}%");
         }
 
@@ -103,11 +80,13 @@ namespace Vote_sys
         static void Main(string[] args)
         {
             // les declarations
-            var yes = new Counter("Yes", 4);
-            var no = new Counter("No", 6);
-            var maybe = new Counter("Maybe", 2);
+            var partie_1 = new Counter("P1", 9);
+            var partie_2 = new Counter("P2", 9);
+            var partie_3 = new Counter("P3", 2);
+            var partie_4 = new Counter("P4", 9);
+            var partie_5 = new Counter("P5", 7);
 
-            var manager = new CounterManager(yes, no, maybe);
+            var manager = new CounterManager(partie_1, partie_2, partie_3, partie_4, partie_5);
             manager.AnnounceWinner();
 
 
