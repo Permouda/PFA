dace6bc92a3acfebbabe4abd6b2da21ff9d5c425
Permouda <82866195+Permouda@users.noreply.github.com>
2022-05-06 11:51:12 +0000

    winner part_1_1 final

--- Asp.net_core/Vote_sys/Program.cs
+++ Asp.net_core/Vote_sys/Program.cs
@@ -2,35 +2,71 @@
 
 namespace Vote_sys
 {
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
             int yesCounter = 5;
             int noCounter = 3;
-            
+            int maybeCounter = 6;
+
             // Les pourcentages
-            int total = yesCounter + noCounter;
+            int total = yesCounter + noCounter + maybeCounter;
 
             var yesPercent = Math.Round(yesCounter * 100.0 / total) ;
-            var noPercent = noCounter * 100.0 / total;
+            var noPercent = Math.Round(noCounter * 100.0 / total);
+            var maybePercent = Math.Round(maybeCounter * 100.0 / total);
 
 
 
             Console.WriteLine($"Yes Counter: {yesCounter}, Percentage:{yesPercent}%");
             Console.WriteLine($"No Counter: {noCounter}, Percentage:{noPercent}%");
+            Console.WriteLine($"No Counter: {maybeCounter}, Percentage:{maybePercent}%");
 
             if (yesCounter > noCounter)
             {
-                Console.WriteLine($"Yes Won");
+                if (yesCounter > maybeCounter)
+                {
+                    Console.WriteLine($"yes won");
+
+                }else if (maybeCounter > yesCounter)
+                {
+                    Console.WriteLine($"Maybe won");
+                }
+                else { Console.WriteLine("DRAW");
+                }
+
             }else if (noCounter>yesCounter)
             {
-                Console.WriteLine($"No Won");
+                if (noCounter > maybeCounter)
+                {
+                    Console.WriteLine($"yes won");
 
+                }
+                else if (maybeCounter > noCounter)
+                {
+                    Console.WriteLine($"Maybe won");
+                }
+                else
+                {
+                    Console.WriteLine("DRAW");
+                }
             }
             else
             {
-                Console.WriteLine($"DRAW");
+                if (maybeCounter > yesCounter)
+                {
+                    Console.WriteLine($"Maybe won");
+
+                }
+                else
+                {
+                    Console.WriteLine("DRAW");
+                }
             }
 
         }
